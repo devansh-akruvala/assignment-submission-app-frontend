8361c579b46256d1eecfd0e9bc9584a37a1c66e0
Devansh Akruvala <devanshakruvala@gmail.com>
2023-02-21 14:29:16 +0530

    Implementing Role Based Routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import './App.css';
 import { useLocalState } from './util/useLocalStorage';
 import {
@@ -7,17 +6,33 @@ import {
   Routes,
 } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import jwt_decode from "jwt-decode";
 
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import PrivateRoute from './util/PrivateRoute';
 import AssignmentView from './components/AssignmentView';
+import CodeReviewDashboard from './components/CodeReviewDashboard';
+import { useState } from 'react';
 
 
 
 function App() {
+  
   const [jwt, setJwt] = useLocalState("", "jwt")
+  
+  const getRolesFromJWT=()=>{
+    if(jwt){
+    const decoded = jwt_decode(jwt);
+    return decoded.authorities;
+  }else{
+    return []
+  }
+  }
+  
+  
+  const [roles, setroles] = useState(getRolesFromJWT())
 
 
 
@@ -26,6 +41,10 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/dashboard' element={
+        roles.find((role)=> role==='ROLE_CODE_REVIEWER')?
+        <PrivateRoute>
+        <CodeReviewDashboard />
+        </PrivateRoute>:
         <PrivateRoute>
         <Dashboard />
         </PrivateRoute>}
---
+++ src/components/CodeReviewDashboard.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import { Badge, Button, Card, Col, Row } from "react-bootstrap";
+import fetchData from "../services/fetchServices";
+import { useLocalState } from "../util/useLocalStorage";
+
+const CodeReviewDashboard = () => {
+  const [jwt, setJwt] = useLocalState("", "jwt");
+  const [assignments, setassignments] = useState(null);
+
+  useEffect(() => {
+    fetchData("/api/assignments", "GET", jwt).then((assignmentsData) => {
+      setassignments(assignmentsData);
+    });
+  }, []);
+
+  const createAssignment = () => {
+    fetchData("/api/assignments", "POST", jwt).then((assignment) => {
+      window.location.href = `/assignments/${assignment.id}`;
+    });
+  };
+
+  return (
+    <div style={{ margin: "2rem" }}>
+      <Row>
+        <Col>
+          <div
+            className="d-flex justify-content-end"
+            onClick={() => {
+              setJwt(null);
+              window.location.href = "/login";
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            Logout
+          </div>
+        </Col>
+      </Row>
+      {assignments ? (
+        <div
+          className="d-grid gap-4"
+          style={{ gridTemplateColumns: "repeat(auto-fill,18rem)" }}
+        >
+          {assignments.map((assignment) => (
+            <Card key={assignment.id} style={{ width: "18rem" }}>
+              <Card.Body className="d-flex flex-column justify-content-around">
+                <Card.Title>Assignment #{assignment.number}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  <Badge pill bg="info" style={{ fontSize: "1em" }}>
+                    {assignment.status}
+                  </Badge>
+                </Card.Subtitle>
+                <Card.Text>
+                  <p>
+                    <b>GitHub Url</b>: {assignment.githubUrl}
+                  </p>
+                  <p>
+                    <b>Branch</b>: {assignment.branch}
+                  </p>
+                </Card.Text>
+                <Button
+                  variant="secondary"
+                  onClick={() => {
+                    window.location.href = `/assignments/${assignment.id}`;
+                  }}
+                >
+                  Edit
+                </Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
+
+export default CodeReviewDashboard;
